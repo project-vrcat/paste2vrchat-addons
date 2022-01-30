249e2a4dc2282b083e20802dc7ceb4a7e659661b
CunYu <cunyu@liuli.lol>
2022-01-31 04:21:30 +0800

    feat: 更新Manifest版本為V3

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,7 @@
 import paste from "./paste";
 
-chrome.runtime.onInstalled.addListener((): void => {
-  chrome.storage.local.set({ pauseAllVideo: "true" });
-});
+chrome.runtime.onInstalled.addListener(() =>
+  chrome.storage.local.set({ pauseAllVideo: "true" })
+);
 
-chrome.browserAction.onClicked.addListener(async (tab: chrome.tabs.Tab) => {
-  paste(tab);
-});
+chrome.action.onClicked.addListener((tab: chrome.tabs.Tab) => paste(tab));
--- src/paste.ts
+++ src/paste.ts
@@ -1,20 +1,23 @@
-export default function paste(tab: chrome.tabs.Tab) {
+export default async function paste(tab: chrome.tabs.Tab) {
   if (!tab.url || !tab.id) return;
-  chrome.storage.local.get(["pauseAllVideo"], (res) => {
-    if (res["pauseAllVideo"] === "true")
-      chrome.tabs.executeScript(tab.id!, {
-        code: `document.querySelectorAll("video").forEach(v => v.pause());`,
-      });
+  const items = await chrome.storage.local.get(["pauseAllVideo"]);
+  if (items["pauseAllVideo"] === "true")
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id! },
+      func: pauseAllVideo,
+    });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id! },
+    func: urlSchemePaste,
+    args: [tab.url],
   });
-  urlSchemePaste(tab.url);
 }
 
 function urlSchemePaste(text: string) {
   const u = `p2vrc://paste?text=${encodeURIComponent(text)}`;
-  if (window.open(u) === null)
-    chrome.windows.create({ url: u }, (win) => {
-      setTimeout(() => {
-        win?.id && chrome.windows.remove(win.id);
-      }, 5000);
-    });
+  window.open(u);
+}
+
+function pauseAllVideo() {
+  document.querySelectorAll("video").forEach((v: any) => v.pause());
 }
